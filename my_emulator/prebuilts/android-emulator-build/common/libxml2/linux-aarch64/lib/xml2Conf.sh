9e9bad28524312dc7086f3185904903bb65c439f
qpzmV <849840504@qq.com>
2026-03-31 18:42:58 +0800

    add common

---
+++ my_emulator/prebuilts/android-emulator-build/common/libxml2/linux-aarch64/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/bohu/install/libxml2/lib"
+XML2_LIBS="-lxml2    -lm "
+XML2_INCLUDEDIR="-I/home/bohu/install/libxml2/include/libxml2"
+MODULE_VERSION="xml2-2.9.2"
+
---
+++ my_emulator/prebuilts/android-emulator-build/common/virglrenderer/update-headers.sh
@@ -0,0 +1,7 @@
+cp $1/include/virgl/* linux-x86_64/include/virgl/
+cp $1/include/virgl/* darwin-x86_64/include/virgl/
+cp $1/include/virgl/* darwin-aarch64/include/virgl/
+cp $1/include/virgl/* linux-aarch64/include/virgl/
+cp $1/include/virgl/* windows_msvc-x86_64/include/virgl/
+cp $1/include/virgl/* windows-x86/include/virgl/
+cp $1/include/virgl/* windows-x86_64/include/virgl/
